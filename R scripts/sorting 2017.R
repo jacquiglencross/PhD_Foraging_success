b492a98ddcd8b40b0d1bb321370167ae5494deb7
Jacqui Glencross <jg287@st-andrews.ac.uk>
2023-02-28 17:42:21 +0000

    still not working

--- R scripts/sorting 2017.R
+++ R scripts/sorting 2017.R
@@ -1,4 +1,12 @@
-setwd("E:/raw_penguin/Robben/AXY_Raw/2017R_AXY_Raw")
+pacman::p_load(tidyverse, dplyr, lubridate, stringr, purrr, #tidyr
+               data.table, imputeTS, #datawrangling
+               foreach, doParallel, furrr, progressr, #parallel
+               sf, crawl, diveMove, forecast, zoo, #movement analysis
+               ggplot2, #datavis
+               here, beepr) #other useful packages 
+
+
+setwd("D:/raw_penguin/Robben/AXY_Raw/2017R_AXY_Raw")
 
 #df <- read.csv("C:/Users/jg287/Downloads/AP_RI_D1_EW079-17_08062017_RI01_S1.csv")
 
@@ -13,22 +21,29 @@ axy <- read.csv(paste0("01_2017R_axytdr.csv")) %>%   # read in axy data
   #mutate(DateTimeOS = paste(Date,Time, sep=" ")) %>% #Make DateTime column
   #mutate(DateTime = dmy_hms(DateTimeOS)) %>% 
   dplyr::select(X, Y, Z, Date, Time, TagID) %>%#, DateTime,  DateTimeOS)
-  mutate(deployID = i)  #and remove file suffix
+  mutate(deployID = i) %>% #and remove file suffix
+  mutate(Date = dmy(Date)) %>%
+  mutate(Time1 = paste(00, Time, sep=":")) %>%
+  mutate(Time1 = lubridate::hms(Time1, roll =FALSE))
 
+summary(axy$Date)
+## Using TDR won't work because multiple minutes:seconds
+#tdr <- read.csv(paste0("D:/raw_penguin/Robben/TDR_Raw/2017R_TDR_Raw/01_2017R_tdr.csv")) %>%
+#  mutate(Date = dmy(Date)) %>%
+#  mutate(deployID = i,
+#         hour = as.character(format(strptime(Time, "%H:%M:%S"),"%H")),
+#         minsec = format(strptime(Time, "%H:%M:%S"),"%M:%S")) %>%
+#  rename(., "Timetdr" = "Time") %>%
+#  select(-Pressure, -Temp)
+#axy2 <- axy1 %>%
+  #merge(x=.,y=tdr,by=c("minsec", "Date"),all.x=TRUE)
+#  left_join(., tdr)
 
-
-tdr <- read.csv(paste0("E:/raw_penguin/Robben/TDR_Raw/2017R_TDR_Raw/01_2017R_tdr.csv")) %>%
-  mutate(Date = as.POSIXct(Date, format ="%d/%m/%Y")) %>%
-  mutate(deployID = i,
-         hour = as.character(format(strptime(Time, "%H:%M:%S"),"%H")),
-         minsec = format(strptime(Time, "%H:%M:%S"),"%M:%S")) %>%
-  rename(., "Timetdr" = "Time") %>%
-  select(-Pressure, -Temp)
-
-
-#metadata <- read.csv("D:/Chapter 4 - foraging success/Metadata_MASTER.csv")%>%
-#  select(deployID,  AXY_starttime) %>%
-#  filter(deployID == i)
+metadata <- read.csv("D:/Chapter 4 - foraging success/Metadata_MASTER.csv")%>%
+  select(deployID,  AXY_starttime) %>%  
+  filter(deployID == i) %>%
+  mutate(AXY_starttime1 = lubridate::hms(AXY_starttime, roll =FALSE))# %>%
+ # mutate(AXYhour = format(strptime(AXY_starttime, "%H:%M:%S"),"%H"))
 
 
 #hours <- metadata  %>% 
@@ -36,43 +51,48 @@ tdr <- read.csv(paste0("E:/raw_penguin/Robben/TDR_Raw/2017R_TDR_Raw/01_2017R_tdr
 
 options(digits.secs = 3) 
 
-#newhour <- ("2017-06-08 00:00:00.0")
-  
+starttime <- ("14:31:11.040")
+#addtime <- ("00:00:00.020")
   
 axy1 <- axy %>%
-  #mutate(StartTimeOS = metadata$AXY_starttime) %>%
-  #mutate(StartDateTime = dmy_hms(paste(Date[1], StartTimeOS))) %>%
-  mutate(Date = as.POSIXct(Date, format ="%d/%m/%Y")) %>% 
-  mutate(Time1 = paste(00, Time, sep=":")) %>%  
-  mutate(minsec = format(strptime(Time1, "%H:%M:%S"),"%M:%S")) %>%
-  merge(x=.,y=tdr,by=c("minsec", "Date"),all.x=TRUE)
-  left_join(., tdr)
+  mutate(rownum = 1:nrow(.)) %>%
+  mutate(NewTime = ifelse(rownum == 1 , starttime, NA),
+         NewTime = hms(NewTime)) 
+axy2 <- axy1[1:100,]
 
-head(axy1)
-
-  mutate(DateTime = as.POSIXct(paste(Date,Time1))) %>%
-  mutate(newhour = ifelse(DateTime == newhour, TRUE, FALSE)) %>%
-  mutate(hourdiff = rle(newhour)$lengths %>% {rep(seq(length(.)), .)})         #calculate time elapsed between start and end
-
-axy2 <- axy1 %>%
-  mutate(hour = as.numeric(hours$axyhour) + as.numeric(hourdiff) - 1) %>%
-  mutate(NewTime = paste(hour, Time, sep=":")) %>%
-  select(Date, NewTime, X, Y, Z, TagID) %>%
-  dplyr::rename(., "Time" = "NewTime") 
-
-axy3 <- axy2 %>%
-  mutate(DateTime = as.POSIXct(paste(Date,Time))) %>%
-  group_by(DateTime) %>%
-  mutate(counts = row_number())
+testing <- axy2 %>%
+   mutate(NewTime1 = accumulate(NewTime, ~ .x + seconds(0.02))) #accumulate is the function from purrr
 
 
-axy2 <- axy1 %>%
-  #mutate(OS = as.numeric(format(strptime(TimeOS, "%H:%M:%OS"),"%OS"))) %>%
-  #mutate(decimals = OS %% 1) %>%
-  mutate(DateTimeOS = paste(Date,TimeOS, sep=" ")) %>%
+str(axy2$NewTime)
+
+axy2$NewTime1 = vector(mode = "numeric", length = length(axy2$NewTime))
+axy2$NewTime1[1] = axy2$NewTime[1]
+for(idx in 2:length(axy2$NewTime)) {
+  axy2$NewTime1[idx] = axy2$NewTime1[idx - 1] + seconds(0.02)
+}
+axy2[ , "NewTime" := seconds(0.02) + shift(B, 1L, type="lag")]
+
+templist1 = list()
+#i <- 2
+
+
+for (i in 2:nrow(axy1)) { 
+  lagTime <- axy1 %>% 
+    select(rownum, NewTime) %>%
+    filter(rownum == i-1)
+  
+  new<- axy1 %>% 
+    filter(rownum == 1) %>%
+    mutate(NewTime1 = lagTime$NewTime + seconds(0.02))
   
 
-summary(axy2$counts)
+  templist1[[i]] <- new  
+  print("done")
+}
+axy3 <- data.table::rbindlist(templist1) 
+
+
 
 
 write.csv(axy1, file = "D:/raw_penguin/Robben/AXY_Raw/2017R_AXY_Rawnew/01_2017R_axytdr.csv")
